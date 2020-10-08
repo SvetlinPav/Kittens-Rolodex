7f8b5447d48c77bf9ed7c26eae2ab05fc69e9732
Svetlin Pavlov <svetlinpav@svetlinpav@gmail.com>
2020-10-08 21:45:09 +0300

    Updates

---
+++ precache-manifest.0e032a68ad7323101984158489dee3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09b12fc137db32e968c88dc774ef03b",
+    "url": "/Kittens-Rolodex/index.html"
+  },
+  {
+    "revision": "96ac872ffb71b7b0f252",
+    "url": "/Kittens-Rolodex/static/css/main.c8f88333.chunk.css"
+  },
+  {
+    "revision": "f390e675dd2e08c1e237",
+    "url": "/Kittens-Rolodex/static/js/2.8302afc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kittens-Rolodex/static/js/2.8302afc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ac872ffb71b7b0f252",
+    "url": "/Kittens-Rolodex/static/js/main.585bf58b.chunk.js"
+  },
+  {
+    "revision": "4988c9276b4525c02796",
+    "url": "/Kittens-Rolodex/static/js/runtime-main.b6d0cb7b.js"
+  }
+]);
